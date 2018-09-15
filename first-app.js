10daff40ad0bb0bb5afa36776785dd9c55d236ba
im_yasinashraf <yasin.ashraf@storilabs.com>
2018-09-15 16:00:16 +0530

    first push

---
+++ first-app.js
@@ -0,0 +1,10 @@
+const log = require('./logger');
+
+function Hello(name){
+    console.log("Hello "+ name);
+}
+
+var message = "Yasin";
+Hello(message);
+
+log('this is a log message');
---
+++ logger.js
@@ -0,0 +1,8 @@
+var url = "http://www.yasinashraf.com";
+
+function log(message){
+    console.log(message);
+}
+
+module.exports = log;
+
