2b7964d205db5ee3f5f81fde3aa823b5c469218e
im_yasinashraf <yasin.ashraf@storilabs.com>
2018-09-27 21:22:53 +0530

    upto file system

--- first-app.js
+++ first-app.js
@@ -8,3 +8,24 @@ var message = "Yasin";
 Hello(message);
 
 log('this is a log message');
+
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+
+log(pathObj);
+
+const os = require('os');
+var osMem = os.totalmem();
+var osFreeMem = os.freemem();
+
+console.log(`Total Memory: ${osMem}`);
+console.log(`Free Memory: ${osFreeMem}`);
+
+
+var total = (osMem + osFreeMem);
+log(`Toal of these two = ${total}`);
+
+const fs = require('fs');
+var files = fs.readdirSync('./');
+log(files);
